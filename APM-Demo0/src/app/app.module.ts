34acb4d8fb351de90c181b560fc71cdc10fa019f
Moe Green <gearmobile@gmail.com>
2019-09-15 21:20:15 +0300

    feat: add redux devtools

--- APM-Demo0/src/app/app.module.ts
+++ APM-Demo0/src/app/app.module.ts
@@ -17,6 +17,8 @@ import { PageNotFoundComponent } from './home/page-not-found.component';
 /* Feature Modules */
 import { UserModule } from './user/user.module';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   imports: [
@@ -26,6 +28,11 @@ import { StoreModule } from '@ngrx/store';
     UserModule,
     AppRoutingModule,
     StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument({
+      name: 'First NgRx App',
+      maxAge: 20,
+      logOnly: environment.production,
+    }),
   ],
   declarations: [
     AppComponent,
