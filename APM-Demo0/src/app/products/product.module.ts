34e1f9c3a9b7baf5cbc2f3eb6c0ed4d6e6dd192d
Moe Green <gearmobile@gmail.com>
2019-09-15 20:29:44 +0300

    feat: add checkProductCose state

--- APM-Demo0/src/app/products/product.module.ts
+++ APM-Demo0/src/app/products/product.module.ts
@@ -6,20 +6,17 @@ import { SharedModule } from '../shared/shared.module';
 import { ProductShellComponent } from './product-shell/product-shell.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
+import { StoreModule } from '@ngrx/store';
+import { reducer } from './state/product.reducer';
 
-const productRoutes: Routes = [
-  { path: '', component: ProductShellComponent }
-];
+const productRoutes: Routes = [{ path: '', component: ProductShellComponent }];
 
 @NgModule({
   imports: [
     SharedModule,
-    RouterModule.forChild(productRoutes)
+    RouterModule.forChild(productRoutes),
+    StoreModule.forFeature('products', reducer),
   ],
-  declarations: [
-    ProductShellComponent,
-    ProductListComponent,
-    ProductEditComponent
-  ]
+  declarations: [ProductShellComponent, ProductListComponent, ProductEditComponent],
 })
-export class ProductModule { }
+export class ProductModule {}
