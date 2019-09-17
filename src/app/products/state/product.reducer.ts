1ab9cea9ef040310b7dfe711eea28b5cbbeb34fc
Moe Green <gearmobile@gmail.com>
2019-09-17 19:02:37 +0300

    feat: add effects

--- src/app/products/state/product.reducer.ts
+++ src/app/products/state/product.reducer.ts
@@ -69,6 +69,11 @@ export function reducer(
           starRating: 0,
         },
       };
+    case ProductActionTypes.LoadSuccess:
+      return {
+        ...state,
+        products: { ...action.payload },
+      };
     default:
       return state;
   }
