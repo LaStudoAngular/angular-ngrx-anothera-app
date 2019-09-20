b640e38270380678a41956861d3ec45c729c1379
Moe Green <gearmobile@gmail.com>
2019-09-20 16:40:15 +0300

    feat: effects

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -9,17 +9,16 @@ import { Product } from './product';
 })
 export class ProductService {
   private productsUrl = 'api/products';
-  private products: Product[];
+  // private products: Product[];
 
   constructor(private http: HttpClient) {}
 
   getProducts(): Observable<Product[]> {
-    if (this.products) {
-      return of(this.products);
-    }
+    // if (this.products) {
+    //   return of(this.products);
+    // }
     return this.http.get<Product[]>(this.productsUrl).pipe(
-      // tap(data => console.log(JSON.stringify(data))),
-      tap(data => (this.products = data)),
+      // tap(data => (this.products = data)),
       catchError(this.handleError),
     );
   }
@@ -29,7 +28,7 @@ export class ProductService {
     product.id = null;
     return this.http.post<Product>(this.productsUrl, product, { headers }).pipe(
       // tap(data => console.log('createProduct: ' + JSON.stringify(data))),
-      tap(data => this.products.push(data)),
+      // tap(data => this.products.push(data)),
       catchError(this.handleError),
     );
   }
@@ -39,12 +38,12 @@ export class ProductService {
     const url = `${this.productsUrl}/${id}`;
     return this.http.delete<Product>(url, { headers }).pipe(
       // tap(data => console.log('deleteProduct: ' + id)),
-      tap(data => {
-        const foundIndex = this.products.findIndex(item => item.id === id);
-        if (foundIndex > -1) {
-          this.products.splice(foundIndex, 1);
-        }
-      }),
+      // tap(data => {
+      //   const foundIndex = this.products.findIndex(item => item.id === id);
+      //   if (foundIndex > -1) {
+      //     this.products.splice(foundIndex, 1);
+      //   }
+      // }),
       catchError(this.handleError),
     );
   }
@@ -54,27 +53,22 @@ export class ProductService {
     const url = `${this.productsUrl}/${product.id}`;
     return this.http.put<Product>(url, product, { headers }).pipe(
       // tap(() => console.log('updateProduct: ' + product.id)),
-      tap(() => {
-        const foundIndex = this.products.findIndex(item => item.id === product.id);
-        if (foundIndex > -1) {
-          this.products[foundIndex] = product;
-        }
-      }),
+      // tap(() => {
+      //   const foundIndex = this.products.findIndex(item => item.id === product.id);
+      //   if (foundIndex > -1) {
+      //     this.products[foundIndex] = product;
+      //   }
+      // }),
       map(() => product),
       catchError(this.handleError),
     );
   }
 
   private handleError(err) {
-    // in a real world app, we may send the server to some remote logging infrastructure
-    // instead of just logging it to the console
     let errorMessage: string;
     if (err.error instanceof ErrorEvent) {
-      // A client-side or network error occurred. Handle it accordingly.
       errorMessage = `An error occurred: ${err.error.message}`;
     } else {
-      // The backend returned an unsuccessful response code.
-      // The response body may contain clues as to what went wrong,
       errorMessage = `Backend returned code ${err.status}: ${err.body.error}`;
     }
     console.error(err);
